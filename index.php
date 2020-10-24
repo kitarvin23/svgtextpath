6ca41092b0149519c6e4dfeba82c80c56ae10a53
kitarvin23 <60122988+kitarvin23@users.noreply.github.com>
2020-10-24 15:35:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Gallego SVG</title>
+</head>
+<body style="margin-left: 190px;">
+        <svg viewBox="0 0 100 100" style="border: black solid;width:900px; height:780px; background-color:#f1f1f1;"> 
+            
+        <path  id="MyPath" d="M30 10, Q50 -5, 70 10, M70 10, Q85 25, 60 40, V45, H40 , V40 ,M40 40, Q15 25, 30 10, " stroke="none" fill="none" />
+  
+  <path id="MyPath2" d="M40 50, H60 , V60 , T55 63, V65,T60 70, V75, T55 78, V80, L60 85, V90, L50 100, L40 90, Z  " stroke="none" fill="none" />
+    
+	<path id="circle" d="M 9,0 A 9,9 0 0 1 -9,0 A 9,9 0 0 1 9,0" transform="translate(50,20)" fill="none" stroke="none"/>
+  
+  	<path id="success" d="M45 62, v20" fill="none" stroke="none"/>
+
+    <text font-family="Verdana" font-size="3" fill="black" >
+    <textPath href="#MyPath" >The future of a Person can be unpredictable if she lacks education;
+an uneducated person does not have the knowledge
+of what the future has to offer for her.
+Education is an essential tool to make a pleasant and stable living out of a simple quotidian life.
+      
+    </textPath><textPath href="#MyPath2" >The Key To Success - Poem by Sofi Martinez
+Autoplay next videoEducation is relevant for a person to be successful.
+Education empowers a person to become a leader.
+Great opportunities are presented to professionals.
+    </textPath>
+      
+      <textPath href="#circle"> “Everyone holds his or her own key to success and happiness."</textPath>
+      
+      <textPath href="#success" font-size="5.5"  font-family="Comic Sans MS, cursive, sans-serif" font-weight="bold" fill="#005555"> Success</textPath>
+  </text>
+  
+  
+  <text x="45.5" y="6.5" font-size="4" font-family="Comic Sans MS, cursive, sans-serif" 
+        font-weight="bold" fill="#00c1d5" >THE</text>
+    <text x="47" y="40" font-size="6" font-family="Comic Sans MS, cursive, sans-serif" 
+        font-weight="bold" fill="#00c1d5">KEY</text>
+    <text x="44" y="60" font-size="6" font-family="Comic Sans MS, cursive, sans-serif" 
+        font-weight="bold" fill="#00c1d5">OF</text>
+  
+  
+  
+  
+     <text x="55" y="7" font-size="3" font-family="Verdana" 
+         fill="#000">Keys</text>  
+     <text x="39" y="7" font-size="3" font-family="Verdana" 
+         fill="#000">-Just</text>  
+     <text x="34" y="10" font-size="3" font-family="Verdana" 
+         fill="#000">be yourself, be who y-</text>
+  <text x="29" y="13" font-size="3" font-family="Verdana" 
+         fill="#000">ou are..</text>
+  <text x="58" y="13" font-size="3" font-family="Verdana" 
+         fill="#000">Love is -</text>
+   <text x="59" y="16" font-size="3" font-family="Verdana" 
+         fill="#000">The key..</text>
+  <text x="60" y="19" font-size="3" font-family="Verdana" 
+         fill="#000">To success</text>
+   <text x="26" y="17" font-size="3" font-family="Verdana" 
+         fill="#000">Failure is</text>
+    <text x="60.5" y="22" font-size="3" font-family="Verdana" 
+         fill="#000">Challenge</text>
+   <text x="60.5" y="25" font-size="3" font-family="Verdana" 
+         fill="#000">is everyw</text>
+  <text x="59" y="28" font-size="3" font-family="Verdana" 
+         fill="#000">here.Nev</text>
+    <text x="56" y="31" font-size="3" font-family="Verdana" 
+         fill="#000">er give up</text>
+   <text x="25" y="20" font-size="3" font-family="Verdana" 
+         fill="#000">not a sin</text>
+  <text x="25" y="24" font-size="3" font-family="Verdana" 
+         fill="#000">failure te-</text>
+   <text x="25" y="27" font-size="3" font-family="Verdana" 
+         fill="#000">aches you</text>
+   <text x="27" y="30" font-size="3" font-family="Verdana" 
+         fill="#000">how to suc</text>
+  <text x="29" y="33" font-size="3" font-family="Verdana" 
+         fill="#000">cess failure helps you from</text>
+   <text x="32" y="35.5" font-size="3" font-family="Verdana" 
+         fill="#000">failing,failure always in  </text>
+   <text x="37" y="39" font-size="3" font-family="Verdana" 
+         fill="#000">failure </text>
+    <text x="58" y="39" font-size="2.5" font-family="Verdana" 
+         fill="#000">Try! </text>
+ <text x="41" y="43" font-size="3" font-family="Verdana" 
+         fill="#000">self love is.. </text>
+   <text x="41" y="53" font-size="2.5" font-family="Verdana" 
+         fill="#000">very important </text>
+     <text x="41" y="55" font-size="2.5" font-family="Verdana" 
+         fill="#000">You Learn More </text>
+   <text x="40.5" y="57" font-size="2.5" font-family="Verdana" 
+         fill="#000">be</text>
+     <text x="40.5" y="59" font-size="2.5" font-family="Verdana" 
+         fill="#000">be</text>
+     <text x="52" y="57" font-size="2.5" font-family="Verdana" 
+         fill="#000">Have -</text>
+     <text x="52" y="59" font-size="2.5" font-family="Verdana" 
+         fill="#000">happy</text>
+       <text x="40.5" y="62" font-size="2.5" font-family="Verdana" 
+         fill="#000">Continue to gr</text>
+    <text x="40.5" y="64" font-size="2.5" font-family="Verdana" 
+          fill="#000">life  <tspan fill="none">....</tspan> is -</text>
+    <text x="40.5" y="66" font-size="2.5" font-family="Verdana" 
+          fill="#000">alw  <tspan fill="none">...</tspan> ays</text>
+    <text x="40.5" y="68" font-size="2.5" font-family="Verdana" 
+          fill="#000">wor  <tspan fill="none">..</tspan> th. it.</text>
+    <text x="40.5" y="70" font-size="2.5" font-family="Verdana" 
+          fill="#000">let  <tspan fill="none">..</tspan> your heart</text>
+    <text x="40.5" y="72" font-size="2.5" font-family="Verdana" 
+          fill="#000">dec  <tspan fill="none">..</tspan>ide on you</text>
+    <text x="40.5" y="74" font-size="2.5" font-family="Verdana" 
+          fill="#000">let  <tspan fill="none">..</tspan> go your..</text>
+    <text x="40.5" y="76" font-size="2.5" font-family="Verdana" 
+          fill="#000">min  <tspan fill="none">..</tspan> d, follow</text>
+    <text x="40.5" y="78" font-size="2.5" font-family="Verdana" 
+          fill="#000">wha  <tspan fill="none">.</tspan> ts the</text>
+    <text x="40.5" y="80" font-size="2.5" font-family="Verdana" 
+          fill="#000">bes  <tspan fill="none">..</tspan>t for u</text>
+    <text x="40.5" y="82" font-size="2.5" font-family="Verdana" 
+          fill="#000">ma  <tspan fill="none">..</tspan>ke your</text>
+    <text x="40.5" y="84" font-size="2.5" font-family="Verdana" 
+          fill="#000">life  <tspan fill="none"></tspan>a masterpi</text>
+    <text x="40.5" y="86" font-size="2.5" font-family="Verdana" 
+          fill="#000">ece  <tspan fill="none"></tspan> imagine no</text>
+    <text x="40.5" y="88" font-size="2.5" font-family="Verdana" 
+          fill="#000">limitations  <tspan fill="none"></tspan> on..</text>
+    <text x="40.5" y="90" font-size="2.5" font-family="Verdana" 
+          fill="#000">what you can be  <tspan fill="none"></tspan></text>
+    <text x="40.5" y="92" font-size="2.5" font-family="Verdana" 
+          fill="#000">have or do...  <tspan fill="none"></tspan></text>
+    <text x="40.5" y="94" font-size="2.5" font-family="Verdana" 
+          fill="#000"> <tspan fill="none">...</tspan>success is</text>
+    <text x="40.5" y="96" font-size="2" font-family="Verdana" 
+          fill="#000"> <tspan fill="none">...</tspan>everything</text>
+    <text x="40.5" y="98" font-size="4" font-family="Verdana" 
+          fill="#000"> <tspan fill="none">.....</tspan>...</text>
+        </svg>
+
+
+</body>
+</html>
